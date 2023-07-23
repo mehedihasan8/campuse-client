dcc43df81b89c36c27ca7e815626b094538f0cc9
mehedihasan8 <mehedihasanfoysal22@gmail.com>
2023-07-23 14:14:06 +0600

    error page create

--- src/Routes/routes.jsx
+++ src/Routes/routes.jsx
@@ -2,11 +2,13 @@ import { createBrowserRouter } from "react-router-dom";
 import Main from "../Layout/Main";
 import App from "../App";
 import Home from "../pages/Home/Home/Home";
+import Error from "../pages/Sharied/Error";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Main></Main>,
+    errorElement: <Error />,
     children: [
       {
         path: "/",
