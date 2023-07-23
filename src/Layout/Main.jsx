f1f1d15f305c82455665de0332d573b6820a769c
mehedihasan8 <mehedihasanfoysal22@gmail.com>
2023-07-23 12:25:06 +0600

    Create NavBar

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,9 +1,10 @@
 import { Outlet } from "react-router-dom";
+import NavBar from "../pages/Sharied/NavBar";
 
 const Main = () => {
   return (
     <div>
-      main
+      <NavBar />
       <Outlet />
     </div>
   );
